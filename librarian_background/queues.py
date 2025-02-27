e9fe9e06572f36a6bf7ebe287416cf0e2ba3f29a
Ioannis Paraskevakos <iparask@princeton.edu>
2025-02-27 15:17:27 -0500

    formatting

--- librarian_background/queues.py
+++ librarian_background/queues.py
@@ -205,7 +205,7 @@ def check_on_consumed(
             elif current_status == TransferStatus.FAILED:
                 logger.info("Transfer for {q.id} has failed", q=queue_item)
                 queue_item.async_transfer_manager.fail_transfer(server_settings)
-                queue_item.fail(session=session)                
+                queue_item.fail(session=session)
             else:
                 logger.error(
                     "Incompatible return value for transfer status from "
